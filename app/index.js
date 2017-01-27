1bbce01f0306c5ff1c1e8be7ee92f63d7a12264f
Wintory <willyvongga@hotmail.com>
2017-01-27 15:21:34 +0700

    a

--- app/index.js
+++ app/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 import City from './indexpic';
 
+
+
 ReactDOM.render(
 	<City />, 
-	document.getElementById('citycard')
+	document.getElementById('citypic')
 	);
+
+
