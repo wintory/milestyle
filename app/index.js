b80059433ff2978fd801c36bed47c4a1632043de
Wintory <willyvongga@hotmail.com>
2017-01-27 13:33:00 +0700

    subm2

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import HelloWorld from './HelloWorld';
+import City from './indexpic';
 
 ReactDOM.render(
-	<HelloWorld />, 
-	document.getElementById('app')
+	<City />, 
+	document.getElementById('citycard')
 	);
---
+++ app/indexpic.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class City extends Component{
+
+    
+	
+
+
+    render() {
+        return(
+            <div >
+                <img src=""></img>
+                </div>
+            )	;
+    }
+}
